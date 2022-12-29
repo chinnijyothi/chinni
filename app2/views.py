1cea406bf8302c8c2d3e94794dd9b9365755fb05
chinnijyothi <kjyothi895@gmail.com>
2022-12-29 14:51:33 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def Prabhas(request):
+    return HttpResponse('<H1><marquee>MY fAVOURITE HERO</H1></marquee>')
